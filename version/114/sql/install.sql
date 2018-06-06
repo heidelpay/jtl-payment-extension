c0b1c70952b323b2f3ae82476c7e009e4768ef39
David.Owusu <david.owusu@heidelpay.de>
2018-06-06 15:10:28 +0200

    [Refactor](JTL-11) Handle an issue with update to version 114. Sofortüberweisung gets deactivated. Removed Santander from supported payment methods

--- version/114/sql/install.sql
+++ version/114/sql/install.sql
@@ -19,3 +19,36 @@ CREATE TABLE `xplugin_heidelpay_standard_finalize` (
   `cshort_id` CHAR( 14 ) NOT NULL,
   `kBestellung` VARCHAR( 20 ) NOT NULL
 ) ENGINE = INNODB ;
+
+
+/**
+ * Fixing update to 114
+ */
+
+/* Find Plugin with old name */
+SET @old_plugin_name = (SELECT `cModulId`
+                        FROM `tzahlungsart`
+                        WHERE `cModulId`
+                              LIKE 'kPlugin\_%\_heidelpaysofort%berweisungplugin');
+
+/* Identify the plugin number */
+SET @number_start = LOCATE('_', @old_plugin_name) + 1;
+SET @number_end = LOCATE('_', @old_plugin_name, @number_start + 1) - 1;
+SET @plugin_number = SUBSTR(@old_plugin_name, @number_start, @number_end - @number_start + 1);
+
+/* If no Sofortüberweisung exist */
+SET @new_plugin_name = if(
+    ISNULL(@old_plugin_name),
+    (
+      SELECT `cModulId`
+      FROM `tzahlungsart`
+      WHERE `cModulId`
+            LIKE 'kPlugin\_%\_heidelpaysofortplugin'),
+    CONCAT('kPlugin_', @plugin_number, '_heidelpaysofortplugin'
+    )
+);
+
+/* Set the correct cModulId for Sofort and deactivate Sofortüberweisung*/
+UPDATE `tzahlungsart` SET `cModulId` = @new_plugin_name WHERE `cModulId` LIKE 'kPlugin\__%\_heidelpaysofortplugin';
+UPDATE `tzahlungsart` SET `nActive` = 0 WHERE `cModulId` LIKE 'kPlugin\_%\_heidelpaysofort%berweisungplugin';
+
